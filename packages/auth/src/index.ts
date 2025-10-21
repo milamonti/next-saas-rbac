617e03eb30df1702f07c79802d29b74031b081f8
Lorenzo Milamonti <milamontilorenzo@gmail.com>
2025-10-21 18:16:59 -0300

    .

---
+++ packages/auth/src/index.ts
@@ -0,0 +1,19 @@
+import { AbilityBuilder, createMongoAbility } from '@casl/ability';
+import type { CreateAbility, ForcedSubject, MongoAbility } from '@casl/ability';
+
+const actions = ['manage', 'invite', 'delete'] as const;
+const subjects = ['User', 'all'] as const;
+type AppAbilities = [
+  typeof actions[number],
+  typeof subjects[number] | ForcedSubject<Exclude<typeof subjects[number], 'all'>>
+];
+
+export type AppAbility = MongoAbility<AppAbilities>;
+export const createAppAbility = createMongoAbility as CreateAbility<AppAbility>;
+
+const { build, can, cannot } = new AbilityBuilder(createAppAbility);
+
+can('invite', 'User');
+cannot('invite', 'User')
+
+export const ability = build();
